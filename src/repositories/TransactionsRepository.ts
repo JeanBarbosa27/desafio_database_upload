44495c3eacb85f9406278a8a1001b698ccc3af44
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-05-14 11:48:59 -0300

    Lógica do getBalance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
@@ -8,10 +8,45 @@ interface Balance {
   total: number;
 }
 
+interface ReducerReturn {
+  value: number;
+}
+
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactionsRepository = getRepository(Transaction);
+    const transactions = await transactionsRepository.find();
+
+    const reducer = (type: 'income' | 'outcome'): ReducerReturn => {
+      const transactionsFiltered = transactions.filter(
+        transaction => transaction.type === type
+      );
+
+      const transactionsReduced = transactionsFiltered.length
+        ? transactionsFiltered.reduce(
+          (current, accumulator) => {
+            return {
+              value: current.value + accumulator.value,
+            }
+          },
+          { value: 0 }
+        )
+        : { value: 0 }
+
+      return transactionsReduced;
+    }
+
+    const income = reducer('income').value;
+    const outcome = reducer('outcome').value;
+    const total = income - outcome;
+    const balance = {
+      income,
+      outcome,
+      total,
+    }
+
+    return balance;
   }
 }
 
