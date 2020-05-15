4b8fadcf9510d746afe3d540330c73bd8d8c01a0
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-05-15 14:01:54 -0300

    Serviço de criar a transação

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,64 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import AppError from '../errors/AppError';
+
+interface RequestDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: RequestDTO): Promise<Transaction> {
+    const transactionsRepository = getRepository(Transaction);
+    const categoriesRepository = getRepository(Category);
+
+    if (!title || !value || !type || !category) {
+      throw new AppError(
+        'All fields (title, value, type and category) are required!',
+      );
+    }
+
+    const findCategory = await categoriesRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    let categoryId = findCategory?.id;
+
+    if (!categoryId) {
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      const categorySaved = await categoriesRepository.save(newCategory);
+      categoryId = categorySaved.id;
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryId,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    delete transaction.id;
+
+    return transaction;
   }
 }
 
