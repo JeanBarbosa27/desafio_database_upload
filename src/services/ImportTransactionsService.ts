edc63cf6ccbd51556aa71c915ab8cb70d8767d7a
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-05-15 18:19:50 -0300

    Testando lógica para salvar diversas transações

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,57 @@
+import { getRepository } from 'typeorm';
+
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
 class ImportTransactionsService {
   async execute(): Promise<Transaction[]> {
-    // TODO
+    const transactionsRepository = getRepository(Transaction);
+    const categoriesRepository = getRepository(Category);
+
+    // TODO: Ler o arquivo enviado e fazer um loop para instanciar uma transação para cada item do arquivo e por fim, salvar todos os items na entidade.
+    const registries = [
+      {
+        title: 'Salário',
+        value: 1000,
+        type: 'income',
+        category: 'Ativos',
+      },
+      {
+        title: 'NET',
+        value: 340,
+        type: 'outcome',
+        category: 'Habitação',
+      },
+    ];
+    const transactions = [] as Transaction[];
+
+    registries.map(async registry => {
+      const { title, value, type, category } = registry;
+
+      const categoryEntity = await categoriesRepository.findOne({
+        where: { category },
+      });
+
+      const categoryId = categoryEntity?.id;
+
+      if (!categoryId) {
+        const newCategory = categoriesRepository.create({ title: category });
+        await categoriesRepository.save(newCategory);
+      }
+
+      const registryEdited = {
+        title,
+        value,
+        type,
+        category_id: categoryId,
+      };
+      const transaction = transactionsRepository.create(registryEdited);
+      return transactions.push(transaction);
+    });
+
+    await transactionsRepository.save(transactions);
+
+    return transactions;
   }
 }
 
