7cc8b73f4edee161cb67085adbc9390b9d9c4402
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-05-15 16:18:16 -0300

    Cria serviço de deletar transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+
+import Transaction from '../models/Transaction';
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getRepository(Transaction);
+    const transaction = await transactionsRepository.findOne({ where: { id } });
+
+    if (!transaction) {
+      throw new AppError('No transactions found with this id!');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
